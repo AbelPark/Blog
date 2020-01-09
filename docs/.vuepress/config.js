45f9bfbfdf8cfe306ae1c2b2f2e0d034f89f727c
parkhm0829 <parkhm0829@gmail.com>
2020-01-09 09:47:21 +0900

    config changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,49 +8,49 @@ module.exports = {
   },
   themeConfig: {
     nav: [
-      {text: 'Frontend', link: '/01frontend/'},
-      {text: 'Backend', link: '/02backend/'},
-      {text: 'DataBase', link: '/03database/'},
-      {text: 'ETC.', link: '/04etc/'},
-      {text: 'list', link: '/05list/'},
-      {text: 'I\'m', link: '/06im/'}
+      {text: 'Frontend', link: '/frontend/'},
+      {text: 'Backend', link: '/backend/'},
+      {text: 'DataBase', link: '/database/'},
+      {text: 'ETC.', link: '/etc/'},
+      {text: 'list', link: '/tags/'},
+      {text: 'I\'m', link: '/im/'}
     ],
     sidebar: {
-      '/01frontend/': [
+      '/frontend/': [
         {title: 'Vuejs',
           children: [
-            {title: '개발환경 설정하기', path: '/01frontend/01vuejs/01vue_settings.md'},
+            {title: '개발환경 설정하기', path: '/frontend/vuejs/VueSettings.md'},
           ]},
         {title: 'Javascript',
           children: [
-            {title: '이벤트리스너', path: '/01frontend/02javascript/01js_event_listener.md'},
+            {title: '이벤트리스너', path: '/frontend/javascript/01js_event_listener.md'},
           ]},
         {title: 'Html/css',
           children: [
-            {title: '레이어 중앙 정렬하기', path: '/01frontend/03htmlCss/01ui_fixed.md'},
+            {title: '레이어 중앙 정렬하기', path: '/frontend/html_css/01ui_fixed.md'},
           ]},
       ],
-      '/02backend/': [
+      '/backend/': [
         {title: 'Python',
           children: [
-            {title: '개발환경 설정하기', path: '/02backend/01python/01py_settings.md'},
+            {title: '개발환경 설정하기', path: '/backend/python/01PySettings.md'},
           ]},
       ],
-      '/03database/':[
+      '/database/':[
         {title: 'Mysql',
           children: [
-            {title: '개발환경 설정하기', path: '/03database/01mysql/01sql_settings.md'},
+            {title: '개발환경 설정하기', path: '/database/mysql/SqlSettings.md'},
           ]},
         {title: 'Anaconda',
           children: [
-            {title: '개발환경 설정하기', path: '/03database/02anaconda/01ana_settings.md'},
+            {title: '개발환경 설정하기', path: '/database/anaconda/AnaSettings.md'},
           ]}
       ],
-      '/04etc/':[
+      '/etc/':[
         {title: 'vuepress',
           children: [
-            {title: '시작하기', path: '/04etc/01vuepress/01vp_start.md'},
-            {title: '목록 만들기', path: '/04etc/01vuepress/02vp_list.md'},
+            {title: '시작하기', path: '/etc/vuepress/VpStart.md'},
+            {title: '목록 만들기', path: '/etc/vuepress/VpList.md'},
           ]},
       ],
     }
